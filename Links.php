cb6a4c42095ba8dcad3c5bcd90daa77afb513cfc
Thijmen-Scherders <71122580+Thijmen-Scherders@users.noreply.github.com>
2021-10-05 15:41:55 +0200

    start links.php

---
+++ Links.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   
+</head>
+<body>
+    <p>Dit is een site waar je wat verschillende linkjes kan kiezen voor films</p>
+    <a href="https://www.imdb.com/">imdb.com</a>
+    <a href="https://www.videoland.com/nl/">Videoland</a>
+    <a href="https://www.film.nl/">Film.nl</a>
+</body>
+</html>
