b29da48b8dd0c45187a55b3827478836e14ea217
noormargana <55080316+noormargana@users.noreply.github.com>
2021-10-08 14:17:10 +0200

    h

---
+++ hamers.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Een hamer is een klop- of slagwerktuig. Een hamer bestaat uit een steel, het handvat, met aan het eind de zogenaamde hamerkop.
+
+De vorm en grootte en het materiaal van de hamerkop en de lengte van de steel hangen sterk af van het type hamer. Bij hamers met een ijzeren hamerkop staat vaak het aantal grammen in het metaal geslagen. Voor speciale uitvoeringen, zoals voor percussie-instrumenten, wordt ook de term mallet (uit het Engels) gebruikt.
+
+Typen hamers
+De volgende tabel geeft een overzicht van typen van hamers:
+
+gewone hamer
+Een metalen hamerkop van 50-300 gram op een houten steel van ca. 30 cm lang die onder meer wordt gebruikt om spijkers (draadnagels) in houten voorwerpen te slaan.
+klauwhamer
+Zoals een gewone hamer, maar de achterkant van de hamer heeft een platte V-groef waarin spijkers geklemd kunnen worden. De bovenkant is afgerond zodat de geklemde spijker uit het object gerold kan worden.
+vuist of moker
+Een zware hamer met een vierkante hamerkop van 1000-1500 gram op een steel van 30 cm lang, voor zwaar werk. Met een rubberen opzetstuk wordt een vuisthamer gebruikt om tegels of straatstenen in het zand te leggen voor het maken van bestrating.
+tegelhamer
+Houten hamer met grote houten kop en een korte steel. Om tegels te leggen.
+straathamer
+Een hamer om te straten, met een kromme zijde om stenen door midden te slaan, en een rechte afgeplatte zijde om stenen aan te slaan (bestraten).
+voorhamer
+Een hamer om sloopwerkzaamheden uit te voeren. Een hamerkop van een paar kilo op een steel van een meter lang.
+rubberen hamer
+Een rubberen hamer wordt vooral gebruikt bij het leggen van tegels op grond. Een kleine hamer met een rubberen gewicht die kan worden gebruikt voor voorzichtig werk, zoals het aankloppen van kleine lijmverbindingen in de houtbewerking. Kampeerders gebruiken een rubber hamer om de tentpinnen in de grond te slaan. Door (para)medici wordt soms een kleine hamer gebruikt met een zacht gedeelte. Deze heeft meestal twee gekleurde uiteinden.
+houten hamer
+Een hamer met een houten hamerkop die kan worden gebruikt voor voorzichtig werk zoals het vooruithelpen van een guts of beitel in de houtbewerking. De hamerkop is gemaakt van een harde houtsoort, meestal beukenhout. De steel is gemaakt van een taaie houtsoort. Dit is nodig om de klappen goed op te kunnen vangen. De hamerkop kan rond (cilindervormig) zijn of een blokvorm hebben.
+smidshamer
+Een hamer met een gevormd gewicht die kan worden gebruikt om (heet) ijzer op een aambeeld vorm te geven.
+bikhamer
+Een hamer om te bikken, bijvoorbeeld om slakken te verwijderen, ontstaan bij het elektrisch lassen.
+sleg, slegge of slei
+Een langstelige houten hamer, onder meer in gebruik om wiggen in boomstammen te drijven en palen in de grond te slaan.
+zandhamer
+Een (kunststoffen) hamer met diverse hamerkoppen en grootte, waarvan de kop gevuld is met zand, teneinde de terugslag te verkleinen. Wordt toegepast wanneer het werkstuk met zo min mogelijke beschadiging vervormd moet worden of om lagers op assen te tikken.
+bankhamer
+Een bankhamer heeft aan één kant een iets bolvormig baanvlak en aan de andere kant een pen die haaks staat op de langsrichting van de steel. Hij is verkrijgbaar met een gewicht van 50 tot 2000 gram. Deze hamer wordt veel gebruikt door bank- en plaatwerkers.
+haarhamer
+Een haarhamer is een hamer, waarbij de beide kanten uitlopen in een dun plat vlak. Hiermee wordt een zeis gehaard (scherp gemaakt).
+bolhamer
+Een bol- of bolkophamer is een hamer voor het bewerken van metaal, met name het hameren (strekken) van bijvoorbeeld koper- of zilverplaat.
+bilhamer of zwaaispits
+Een bilhamer is een hamer voor het bewerken van molenstenen. Door steenhouwers wordt deze hamer zwaaispits genoemd en werd voorheen gebruikt voor het ruw voorhakken van blokken natuursteen.
+pletsteenhamer
+Een pletsteenhamer of bouchardhamer is een hamer voor het vlak maken van molenstenen en andere natuursteen
+loodhamer
+Een loodhamer is een hamer die in de beeldhouwkunst gebruikt wordt, bestaande uit een (veelal) koperen of messing band gevuld met lood. De boven- en onderkant van de band zijn open. Met die open – loden – kant wordt geslagen op een beitel. Het lood dempt de slagkracht door de relatieve zachtheid. De koperen band houdt het lood bijeen en is noodzakelijk om de kop aan een (houten) steel te bevestigen. Aldus kan zeer fijn scharreer- of bouchardeerwerk gedaan worden in natuursteen.
+noodhamer
+Een noodhamer is bedoeld om, bijvoorbeeld bij een ongeluk, een ruit in te slaan om een voertuig (trein, auto) of een ruimte snel te kunnen verlaten.
+stemhamer
+Eigenlijk geen hamer maar een sleutel (daarom ook stemsleutel genoemd). Wordt gebruikt bij het stemmen van piano's en andere snaarinstrumenten.
+reflexhamer
+Hamertje met een rubber kop waarmee bij een medisch onderzoek de reflexen, vooral de kniepeesreflex, worden getest.
+krijgshamer
+Hamer die als wapen gebruikt wordt.
+terugslagvrije hamer (dead blow)
+Een hamer gevuld met hagel (lood).</p>
+
+<img src="images/hamer.png" alt="hamer" width="500px">
+</body>
+</html>
---
+++ kettingzagen.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Een kettingzaag bestaat uit drie delen:
+
+Motor: de benzinemotor moet gestart worden met een startkoord (als bij een motormaaier). De motor drijft een kettingrondsel aan, dat zorgt dat de ketting gaat ronddraaien. Bij een elektrische kettingzaag dient men op een knop te drukken om de motor te starten.
+Zaagblad: het zaagblad of "zwaard" zit aan de motorbehuizing vastgemaakt. Het zaagblad heeft over de hele lengte een groef. De functie van de groef is tweeledig. Enerzijds zorgt de groef ervoor dat de ketting rechtuit blijft lopen. Anderzijds zorgt de groef voor vervoer van olie, die de ketting smeert. De groef moet voldoende diep zijn, en vrij zijn van vuil en zand. Dit alles om de ketting er niet uit te laten lopen. Het blad mag niet beschadigd zijn en er mogen geen bramen op zitten.
+Zaagketting: een schakelketting met zaagtanden, ook wel beitels genoemd. Een zaagketting bestaat uit drie onderdelen: de zaagtand, de dieptesteller en de aandrijfschakel. Deze zijn met elkaar verbonden door verbindingsschakels. De zaagtand doet het werk. Deze beitelt het hout uit de zaagsnede. De dieptesteller bepaalt de diepte waarmee de zaagtand door het hout gaat. Staat deze te hoog afgesteld neemt de zaagtand bijna geen hout mee. Staat deze te diep afgesteld kan de zaagtand vastlopen in het hout (ook wel 'agressief' genoemd). De aandrijfschakel loopt door de groef van het zaagblad. Hieruit haalt hij olie om de ketting te smeren.
+Een kettingzaag is onnauwkeurig en levert een vrij brede en rafelige zaagsnede op, maar is door zijn handzaamheid en lange zaagblad uitermate geschikt om gehele bomen om te zagen of tot brandhout te verwerken. De omgezaagde bomen worden daarna soms in zagerijen met lintzagen of cirkelzagen tot planken verwerkt, omdat deze een nettere zaagsnede geven.</p>
+
+<img src="images/kettingzaag.png" alt="kettingzaag">
+</body>
+</html>
