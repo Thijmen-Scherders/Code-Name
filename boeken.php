36d087eaf35614a82ea125a2255a92d2590f5524
noormargana <55080316+noormargana@users.noreply.github.com>
2021-10-08 14:02:04 +0200

    james bond

---
+++ boeken.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<p>Wanneer iemand een heel goed (lang) verhaal schrijft wil diegene natuurlijk graag dat iedereen het kan lezen. Daarom gaat hij of zij naar een uitgever toe. Daar wordt het verhaal gelezen en kijken ze of ze er een boek van willen maken. Wanneer het boek wordt afgekeurd (dat is haast altijd het geval) zal het verhaal dat erin staat nooit bekend raken, maar wanneer het verhaal wordt goedgekeurd maakt de drukkerij er een echt boek van.
+
+Om te beginnen halen ze alle spelfouten eruit. En daarna voegen ze (wanneer dit nog niet gebeurd is) zo nodig illustraties toe. De illustraties zijn de plaatjes in een boek, die over het verhaal gaan. Dat boek krijgt een omslag (de boekband of kortweg band). Op de voorkant daarvan staan zeker: de titel, een plaatje of foto, de naam van de schrijver en de uitgeverij. Ook maken ze een verhaaltje (korte inhoud) voor de achterkant. Dat moet de mensen overhalen ("lekker maken") om het boek ook te gaan kopen. Daarna gaan ze het boek drukken, dit betekent dat er meerdere boeken (exemplaren) met exact hetzelfde verhaal worden gemaakt. Deze boeken worden dan verkocht en door de mensen gelezen. Althans, dat mag je hopen.
+
+Het internet heeft in deze gang van zaken grote verandering gebracht. Je bent nu als het ware je eigen uitgever. Iedereen kan van alles schrijven en iedereen kan het lezen. Als je per se een papieren boek wilt uitgeven, dan heb je daar tegenwoordig ook geen uitgever meer voor nodig. Je kunt je op de computer geschreven tekst inleveren bij een gespecialiseerd bedrijf en er dan zoveel boeken van laten drukken als je wilt. Die moet je dan wel zelf betalen. Printing of publishing on demand (drukken of uitgeven op verzoek) heet dat in het Engels.</p>
+
+<img src="images/books.png" alt="boeken">
+</body>
+</html>
