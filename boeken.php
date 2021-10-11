2da8ae1c4044b6c2d94cbef6d1f97ec853247e93
noormargana <55080316+noormargana@users.noreply.github.com>
2021-10-11 09:25:41 +0200

    h

--- boeken.php
+++ boeken.php
@@ -5,8 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+    <header>
+        <?php require_once 'layouts/header.php'; ?>
+    </header>
 <p>Wanneer iemand een heel goed (lang) verhaal schrijft wil diegene natuurlijk graag dat iedereen het kan lezen. Daarom gaat hij of zij naar een uitgever toe. Daar wordt het verhaal gelezen en kijken ze of ze er een boek van willen maken. Wanneer het boek wordt afgekeurd (dat is haast altijd het geval) zal het verhaal dat erin staat nooit bekend raken, maar wanneer het verhaal wordt goedgekeurd maakt de drukkerij er een echt boek van.
 
 Om te beginnen halen ze alle spelfouten eruit. En daarna voegen ze (wanneer dit nog niet gebeurd is) zo nodig illustraties toe. De illustraties zijn de plaatjes in een boek, die over het verhaal gaan. Dat boek krijgt een omslag (de boekband of kortweg band). Op de voorkant daarvan staan zeker: de titel, een plaatje of foto, de naam van de schrijver en de uitgeverij. Ook maken ze een verhaaltje (korte inhoud) voor de achterkant. Dat moet de mensen overhalen ("lekker maken") om het boek ook te gaan kopen. Daarna gaan ze het boek drukken, dit betekent dat er meerdere boeken (exemplaren) met exact hetzelfde verhaal worden gemaakt. Deze boeken worden dan verkocht en door de mensen gelezen. Althans, dat mag je hopen.
---
+++ songtesten/weAreTheChampions.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+
+<body class="bluepage">
+
+    <header>
+        <?php require_once '../layouts/header.php'; ?>
+    </header>
+
+        <p>I've paid my dues
+    Time after time
+    I've done my sentence
+    But committed no crime
+    And bad mistakes
+    I've made a few
+    I've had my share of sand
+    Kicked in my face
+    But I've come through
+    And we mean to go on and on and on and on
+    We are the champions, my friends
+    And we'll keep on fighting till the end
+    We are the champions
+    We are the champions
+    No time for losers
+    'Cause we are the champions of the World
+    I've taken my bows
+    And my curtain calls
+    You brought me fame and fortune
+    And everything that goes with it
+    I thank you all
+    But it's been no bed of roses
+    No pleasure cruise
+    I consider it a challenge before
+    The human race
+    And I ain't gonna lose
+    And we mean to go on and on and on and on
+    We are the champions, my friends
+    And we'll keep on fighting till the end
+    We are the champions
+    We are the champions
+    No time for losers
+    'Cause we are the champions of the World
+    We are the champions, my friends
+    And we'll keep on fighting till the end
+    We are the champions
+    We are the champions
+    No time for losers
+    'Cause we are the champions of the World</p>
+</body>
+</html>
