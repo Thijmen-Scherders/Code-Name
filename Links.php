cf6a3ff1a57256e7d689fccced1c9f7e25d53cb3
Thijmen-Scherders <71122580+Thijmen-Scherders@users.noreply.github.com>
2021-10-11 09:20:30 +0200

    links met foto erbij

--- Links.php
+++ Links.php
@@ -4,16 +4,29 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
    
 </head>
 <body>
+    <?php require_once 'layouts/header.php'; ?>  
+    <div class="p">
     <p>Dit is een site waar je wat verschillende linkjes kan kiezen voor films</p>
-    <img src="../images/imdb.png" alt="Foto imdb">
-    <a href="https://www.imdb.com/">imdb.com</a>
-    <img src="../images/videoland.png" alt="Foto videoland">
-    <a href="https://www.videoland.com/nl/">Videoland</a>
-    <img src="../images/nlfilm.png" alt="Foto filmnl">
-    <a href="https://www.film.nl/">Film.nl</a>
+    </div>
+   
+   <div class="link">
+       <div class="imdb">
+            <img src="images/imdb.png" alt="Foto imdb">
+            <a href="https://www.imdb.com/">imdb.com</a>
+        </div>
+        <div class="videoland">
+            <img src="images/videoland.png" alt="Foto videoland">
+            <a href="https://www.videoland.com/nl/">Videoland</a>
+        </div>
+        <div class="nlfilm">
+            <img src="images/nlfilm.png" alt="Foto filmnl">
+            <a href="https://www.film.nl/">Film.nl</a>
+        </div>
+    </div>
 </body>
 </html>
 
