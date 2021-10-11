f8b8686fe219c16d9fa71f36fb89d93329931471
mickjacobs2004 <71122388+mickjacobs2004@users.noreply.github.com>
2021-10-11 09:29:53 +0200

    headr and start main

--- index.php
+++ index.php
@@ -1,12 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<body>
-    
-</body>
+
 </html>
 <body>
 <?php require_once 'layouts/header.php'; ?>
+<div class="wrapper">
+    <div class="datum_tijd">
+        
+        
+   <h1> <p>Datum & Tijd<br>
+<?php echo(strftime("%m/%d/%Y %H:%M")); ?></p>
+</h1>
+    </div>
+    <div class="homeimg">
+
+    </div>
+    <div class="contactinformatie">
+    <p>Ms Philadelphia Clay<br>
+5645 Harvest Avenue<br>
+Chickaloon, Connecticut,<br> 06510-4485,<br> US
+Phone:(203) 296-9219<br>
+Email Address:philcl@arvinmeritor.info</p>
+    </div>
+</div>
 
 </body>
 </html>
