17fa802fd2fb98d962a4f99557125b0363ba12e3
AnwarJoumad <71122375+AnwarJoumad@users.noreply.github.com>
2021-10-21 14:11:17 +0200

    button

---
+++ songteksten/songteksten.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="nl">
+
+
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="../css/style.css">
+
+</head>
+        
+    
+<body>  
+<?php require_once '../layouts/header.php'; ?>
+       
+    <div class="songteksten">
+        <A href="queen.php">queen</A>
+        <A href="Quen.php">queen</A>
+        <A href="pluginbaby.php">pluginbaby</A>
+        <A href="plugbaby.php">plugbaby</A>
+        <A href="mybaby.php">mybaby</A>
+        <A href="Muse.php">Muse</A>
+        <A href="Madnes.php">Madnes</A>
+        <A href="ironmaiden.php">ironmaiden</A>  
+    </div>
+</body>
+<footer>
+<?php require_once '../layouts/footer.php'; ?>
+</footer>
+</html>
+    
+
+
