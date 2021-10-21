a2d05b8957bbf291e8d484006cee7df0c2993299
mickjacobs2004 <71122388+mickjacobs2004@users.noreply.github.com>
2021-10-21 14:33:06 +0200

    songtextem.php

--- songteksten/songteksten.php
+++ songteksten/songteksten.php
@@ -12,16 +12,18 @@
     
 <body>  
 <?php require_once '../layouts/header.php'; ?>
-       
+<div class="div">
+       <h1> Songteksten</h1>
+       </div>
+       <br>
     <div class="songteksten">
-        <A href="queen.php">queen</A>
-        <A href="Quen.php">queen</A>
-        <A href="pluginbaby.php">pluginbaby</A>
-        <A href="plugbaby.php">plugbaby</A>
-        <A href="mybaby.php">mybaby</A>
-        <A href="Muse.php">Muse</A>
-        <A href="Madnes.php">Madnes</A>
-        <A href="ironmaiden.php">ironmaiden</A>  
+        <A href="queen.php">Killer Queen -- Queen</A>
+        <A href="Quen.php">Bohemian Rhapsody -- Queen</A>
+        <A href="plugbaby.php">Plug In Baby -- Muse</A>
+        <A href="mybaby.php">Uprising -- My Baby</A>
+        <A href="Muse.php">Paranoia -- Muse</A>
+        <A href="Madnes.php">Madness -- Muse</A>
+        <A href="ironmaiden.php">Transvylania -- Muse</A>  
     </div>
 </body>
 <footer>
