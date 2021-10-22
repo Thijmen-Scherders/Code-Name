0d0ff176023d31e190bdf6b5e969d2488fc409fe
Thijmen-Scherders <71122580+Thijmen-Scherders@users.noreply.github.com>
2021-10-22 14:51:44 +0200

    -n

--- songteksten/ironmaiden.php
+++ songteksten/ironmaiden.php
@@ -19,7 +19,7 @@
    
         <div class="songtext">
             <p><b>
-             Is een insturmentalen song
+             Is een insturmentale song
             </b></p>
         </div>
         
