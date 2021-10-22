9f1a4d10a81d8ab890346edc6c36d54820aa877d
mickjacobs2004 <71122388+mickjacobs2004@users.noreply.github.com>
2021-10-22 14:11:15 +0200

    gereedschap

--- gereedschap/Gereedschap.php
+++ gereedschap/Gereedschap.php
@@ -12,13 +12,15 @@
     
 <body>  
 <?php require_once '../layouts/header.php'; ?>
-       
+<div class="div">
+       <h1> Gereedschap</h1>
+       </div>
     <div class="gereedschap">
         
             
-    <a href="kettingzagen.php">kettingzagen</a>
-    <a href="cirkelzagen.php">cirkelzaken</a>
-    <a href="hamers.php">hamer</a>
+        <a href="kettingzagen.php"><h4>kettingzagen</h4></a>
+        <a href="cirkelzagen.php"><h4>cirkelzaken</h4></a>
+        <a href="hamers.php"><h4>hamer</h4></a>
 
          
     </div>
