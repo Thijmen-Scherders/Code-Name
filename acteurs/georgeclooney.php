99547f374f507e5aaa09c8f884329778149b2088
AnwarJoumad <71122375+AnwarJoumad@users.noreply.github.com>
2021-11-01 09:28:20 +0100

    george clooney

---
+++ acteurs/georgeclooney.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="nl">
+
+
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="../css/style.css">
+
+</head>
+
+<body class="greenpage">
+   
+<?php require_once '../layouts/header.php'; ?>
+<div class="wrapper">
+<div class="container">  
+    <img src="../images/george.jpg" alt="" height="350px" width="auto"> 
+    <p class="george">
+    George Timothy Clooney (born May 6, 1961) is an American actor and filmmaker. He is the recipient of three Golden Globe Awards and two Academy Awards, one for acting in Syriana (2006) and the other for co-producing Argo (2012). In 2018, he was the recipient of the AFI Lifetime Achievement Award.
+    Clooney made his acting debut on television in 1978, and later gained wide recognition in his role as Dr. <br> </br>Doug Ross on the medical drama ER from 1994 to 1999, for which he received two Primetime Emmy Award nominations. While working on ER, he began attracting a variety of leading roles in films, with his breakthrough role in From Dusk till Dawn (1996), and the crime comedy Out of Sight (1998), in which he first worked with director Steven Soderbergh, who would become a long-time collaborator. In 1999, he took the lead role in Three Kings, a well-received war satire set during the Gulf War.
+    In 2001, Clooney's fame widened with the release of his biggest commercial success, Soderbergh's heist comedy remake Ocean's Eleven, the first of what became a trilogy, starring Clooney. 
+</p>
+        
+    
+</div>
+</div> 
+<?php require_once '../layouts/footer.php'; ?>              
+</body>
+
+</html>
