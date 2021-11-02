527ecdf293fac018d3af88a7ff1737a3ba53e37e
mickjacobs2004 <71122388+mickjacobs2004@users.noreply.github.com>
2021-11-02 13:39:07 +0100

    frisdranken

--- dranken/frisdranken.php
+++ dranken/frisdranken.php
@@ -13,8 +13,21 @@
 
 <?php require_once '../layouts/header.php'; ?>
 
-<body>
- 
+<body class="frisdrank">
+ <div class="wrapperfris">
+    <div class="container">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae doloremque dolorum eveniet totam odit debitis inventore veritatis esse quia facere asperiores laborum deserunt aliquid molestias porro, sunt, dolorem perferendis sit!</p>
+    </div>
+    <div class="container">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae doloremque dolorum eveniet totam odit debitis inventore veritatis esse quia facere asperiores laborum deserunt aliquid molestias porro, sunt, dolorem perferendis sit!</p>
+    </div>
+    <div class="container">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae doloremque dolorum eveniet totam odit debitis inventore veritatis esse quia facere asperiores laborum deserunt aliquid molestias porro, sunt, dolorem perferendis sit!</p>
+    </div>
+
+
+
+ </div>
     
 </body>
 <footer>
