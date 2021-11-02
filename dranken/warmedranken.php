5c326a7f8835af721888516c73d1239049a90874
Thijmen-Scherders <71122580+Thijmen-Scherders@users.noreply.github.com>
2021-11-02 14:08:25 +0100

    Warmen dranken af

--- dranken/warmedranken.php
+++ dranken/warmedranken.php
@@ -7,20 +7,37 @@
     
 </head>
 
-
+<body class="purplepage">
 
     
 
 <?php require_once '../layouts/header.php'; ?>
 
-<body>
+
+<body class="frisdrank">
+ <div class="wrapperwarm">
+    <div class="container1">
+        <img src="../images/koffie.jpg" alt="koffieplaatje" width="300px" height="300px">
+        <p><br>Koffie<br> is een meestal warm genuttigde drank, die wordt bereid op basis van water en gedroogde en gebrande pitten van de koffieplant die vanwege hun vorm koffiebonen worden genoemd. Koffie bevat het stimulerende middel cafeïne. De meeste soorten in het geslacht Coffea komen van nature voor in tropisch Afrika en op de eilanden in de Indische Oceaan. Ze vinden hun oorsprong in Ethiopië, Jemen en Soedan.
+</p>
+    </div>
+    <div class="container1">
+        <img src="../images/thee.jpg" alt="Thee plaatje" width="300px" height="300px">
+        <p><br>Thee<br> is een warme drank die wordt gemaakt door infusie van de gedroogde bladeren van de theeplant Camellia sinensis. Alle als thee verkochte producten die niet van deze plant afkomstig zijn (bijvoorbeeld kamillethee of rooibos), zijn dan ook geen echte thee maar kruidenthee.</p>
+    </div>
+    <div class="container1">
+        <img src="../images/latte.jpg" alt="lattefoto" width="300px" height="300px">
+        <p>Een <br>caffè latte<br> of latte is een in veel Europese en Amerikaanse gelegenheden geserveerde koffiedrank, gemaakt met espresso en warme melk. Vermoedelijk ligt de oorsprong in de Verenigde Staten, waar de drank uitgevonden zou zijn om de espresso uit de opkomende koffiemachines wat te verzachten. In Italie wordt de term niet of nauwelijks gebruikt. Wie latte bestelt, krijgt melk.</p>
+    </div>
+
     
- 
+
     
-</div>         
 </div>
 </body>
 <footer>
 <?php require_once '../layouts/footer.php'; ?>
 </footer>
 </html>
+
+
