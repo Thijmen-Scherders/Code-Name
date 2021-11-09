87f14fc03dc8473b82acc190d8eff73fd880670b
noormargana <55080316+noormargana@users.noreply.github.com>
2021-11-09 14:07:05 +0100

    form controller

--- index/controller.php
+++ index/controller.php
@@ -9,6 +9,31 @@
 </head>
 <?php require_once '../layouts/header.php'; ?>
 <body>
-    
+
+<?php
+    require_once '../backend/conn.php';
+    $query = "SELECT * FROM boeken";
+    $statement = $conn->prepare($query);
+    $statement->execute();
+    $boeken = $statement->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<?php foreach($boeken as $boek): ?>
+            
+                
+            <div class="boeken-info"> 
+                <h1><?php echo $boek['titel']; ?></h1>
+                <p><?php echo $boek['auteur']; ?>
+                <p>Beschrijving:<?php echo $boek['beschrijving']; ?>
+                <textarea name="description" id="description" cols="30" rows="10"></textarea>
+                <p>Prijs:<?php echo $boek['prijs']; ?>
+                <input type="text" id="price" name="price">
+                <br>
+                <input type="submit">
+            </div>  
+        
+   
+    <?php endforeach; ?>
+</div>
 </body>
 </html>
