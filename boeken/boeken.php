e0c5211d8bde98cb70c657e2919ef02c839f9716
AnwarJoumad <71122375+AnwarJoumad@users.noreply.github.com>
2021-11-09 12:40:32 +0100

    boeken lijst

--- boeken/boeken.php
+++ boeken/boeken.php
@@ -10,7 +10,35 @@
 <body>
 <?php require_once '../layouts/header.php'; ?>
     
-        
+
+
+
+
+
+<?php
+    require_once '../backend/conn.php';
+    $query = "SELECT * FROM boeken";
+    $statement = $conn->prepare($query);
+    $statement->execute();
+    $boeken = $statement->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<div class="grid-container">
+            <?php foreach($boeken as $boek): ?>
+            
+                
+                    <div class="boeken-info"> 
+                        <h1><?php echo $boek['titel']; ?></h1>
+                        <p><?php echo $boek['auteur']; ?>
+                        <p>Beschrijving:<?php echo $boek['beschrijving']; ?>
+                        <p>Prijs:<?php echo $boek['prijs']; ?>
+                    </div>  
+                
+           
+            <?php endforeach; ?>
+        </div>
+                  
+</div>     
     
 
 
